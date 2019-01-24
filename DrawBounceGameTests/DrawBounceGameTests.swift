2d4d466c7cab55aec539ed7fe0b59aa1ec262240
Henry Pigg <henry.pigg@gmail.com>
2019-01-23 14:16:25 -1000

    Initial Commit

---
+++ DrawBounceGameTests/DrawBounceGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  DrawBounceGameTests.swift
+//  DrawBounceGameTests
+//
+//  Created by Henry Pigg on 1/23/19.
+//  Copyright © 2019 Henry Pigg. All rights reserved.
+//
+
+import XCTest
+@testable import DrawBounceGame
+
+class DrawBounceGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
